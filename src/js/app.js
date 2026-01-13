1a20b3be3da45ede0a85864a50bac18a560d1887
YoshiokaY <yoshioka.y@nadia.bz>
2026-01-13 19:11:39 +0900

    [update]グローバルナビゲーション #

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,9 @@
 import { HogeScript } from "./class/sample.ts";
 import { SmoothScroll } from "./class/SmoothScroll.ts";
-import { Hamburger } from "./class/Hamburger.ts";
+import { GlobalNav } from "./class/GlobalNav.ts";
 
 window.addEventListener("load", () => {
   new HogeScript();
   new SmoothScroll();
-  new Hamburger();
+  new GlobalNav();
 });
