ea3a3a0d843d06d20d8cdb51457dc5fe4fca57ac
YoshiokaY <yoshioka.y@nadia.bz>
2026-06-12 13:07:26 +0900

    [remove]WP機能削除

--- htdocs_wp/wp/wp-content/themes/my-theme/single-news.php
+++
@@ -1,7 +0,0 @@
-<?php get_header(); ?>
-
-<main id="main"> 
-  <div class="contentInner"> <article class="p_news"> <nav class="breadcrumbs" aria-label="パンくずリスト"> <ol> <li> <a href="/">ホーム</a> </li><li> <a href="/sample/">ニュース</a> </li><li> <span aria-current="page">Astroで始める高速サイト構築</span> </li> </ol> </nav> <header class="news_header"> <div class="news_meta"> <time datetime="2025-09-28">2025年9月28日</time> <span class="news_category">お知らせ</span> </div> <h1 class="news_ttl">Astroで始める高速サイト構築</h1> </header> <figure class="news_thumbnail"> <img src="<?= get_template_directory_uri() ?>/_assets/img/sample/sample_001.png.webp" alt="Astroのロゴとコード画面" loading="eager"> </figure> <div class="news_body"></div> </article> </div>
- </main>
-
-<?php get_footer(); ?>
